0063c3ed9152e9c7511be2afcaaea0bd9591f148
Andrei <platonovaudit@yandex.ru>
2022-10-16 19:08:29 +0500

    feat: hanoi pass test

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -3,24 +3,22 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Calculate turns number and time (in seconds) required
  * to solve puzzle
- * 
+ *
  * @param {Number} disks number of disks
  * @param {Number} turnsSpeed speed (in turns/hour)
  * @return {Object} object with props turns (number of turns)
  * and seconds (time in seconds)
  *
  * @example
- * 
+ *
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(disksNumber, turnsSpeed) { 
+function calculateHanoi(disksNumber, turnsSpeed) {
   let result = {}
-  if (disksNumber === 1) {
-    result.turns = 1;
-    result.seconds = turnsSpeed / 60 / 60;
+    result.turns = 2 ** (disksNumber) - 1;
+    result.seconds = Math.trunc(result.turns / (turnsSpeed / 60 / 60));
     return result;
-  }
 }
 
 module.exports = {
